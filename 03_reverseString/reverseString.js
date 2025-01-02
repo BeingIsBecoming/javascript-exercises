d42a288a7b358ed6c156ad1dfe2898a2bb2f89cd
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-01-02 15:22:27 -0800

    Wrote function to reverse strings using array methods and tested it with node

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    return x.split("").reverse().join("");
 };
 
-// Do not edit below this line
 module.exports = reverseString;
