47f0a6ca75032ac9b5d0aed83e974adbe9fc7d2b
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-01-14 00:40:32 -0800

    Wrote and tested leapYears function via Jest.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 };
 
-// Do not edit below this line
 module.exports = leapYears;
+
+// Leap years are years divisible by four
+// However, years divisible by 100 are not 
+// Unless they are divisible by 400 
