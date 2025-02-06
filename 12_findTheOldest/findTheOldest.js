e5f043fa66280e0eb6f05579038ccb2645b2a425
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-02-05 21:25:51 -0800

    Write age sorting function that operates without needing date of death.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.sort((a, b) => {
+        a.yearOfDeath ??= currentYear;
+        b.yearOfDeath ??= currentYear;
+        const ageA = a.yearOfDeath - a.yearOfBirth;
+        const ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageA > ageB ? -1 : 1;
+    })[0];
 };
 
 // Do not edit below this line
