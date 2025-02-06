bf94da007161ad4b82c08420f2e2f2083490023e
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-02-05 17:20:24 -0800

    Write fibonacci function to display number at position n within series.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,9 @@
-const fibonacci = function() {
+// FIBONACCI SEQUENCE INDEX
 
+const fibonacci = (n) => {
+    if (+n < 0) return "INVALID";
+    if (+n === 0) return 0;
+    if (+n === 1) return 1;
+    return fibonacci(+n - 1) + fibonacci(+n - 2);
 };
-
-// Do not edit below this line
 module.exports = fibonacci;
